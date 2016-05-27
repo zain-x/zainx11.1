860f039f799b3d8c0537aea1253b29e5807648e0
TMAcademy <support@zainx.esy.es>
2016-05-27 18:54:21 -0400

    Update ZWord-11-OpenSource.bat

--- ZWord-11-OpenSource.bat
+++ ZWord-11-OpenSource.bat
@@ -1,3 +1,14 @@
 @echo off
 color f0
 title ZWord 11 Open Source
+
+:home
+cls
+echo ³1³ New
+echo ³2³ More
+echo ³3³ Exit
+set /p number=
+
+if %number% == 1 goto 1
+if %number% == 2 goto 2
+if %number% == 3 goto Exit
