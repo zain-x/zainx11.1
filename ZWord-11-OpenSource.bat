89e2f8afefccf91cc89d4bb325c3965dca74586c
TMAcademy <support@zainx.esy.es>
2016-05-30 15:15:48 -0400

    Update ZWord-11-OpenSource.bat

--- ZWord-11-OpenSource.bat
+++ ZWord-11-OpenSource.bat
@@ -12,3 +12,18 @@ set /p number=
 if %number% == 1 goto 1
 if %number% == 2 goto 2
 if %number% == 3 goto Exit
+
+:1
+cls
+set /p n=Name:
+cls
+echo Location:
+echo -----------
+echo ³1³ Desktop
+echo ³2³ Documents
+echo ³2³ Downloads
+echo ³3³ Music
+echo ³4³ Pictures
+echo ³5³ Videos
+echo ³6³ Other
+
