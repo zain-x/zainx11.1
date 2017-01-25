f5d0da76bd11e13bc0832f2cbcccc5786e4248da
TMAcademy <TMAcademy@users.noreply.github.com>
2017-01-25 16:47:34 -0500

    Update Open-Troubleshooter.bat

--- Open-Troubleshooter.bat
+++ Open-Troubleshooter.bat
@@ -19,6 +19,7 @@ goto home
 :update
 cls
 bitsadmin.exe /transfer "Open Troubleshooter Update" https://raw.githubusercontent.com/zain-x/zainxopensource/master/Open-Troubleshooter.bat %USERPROFILE%\Desktop\Open-Troubleshooter.bat
+
 :tc1
 cls
 bitsadmin.exe /transfer "Open Troubleshooter Download" https://raw.githubusercontent.com/zain-x/zainxopensource/master/ZWord-Open.bat %USERPROFILE%\Desktop\ZWord-Open.bat
