58e0dcf12ef07665cf173351e4dc46da74767344
TMAcademy <TMAcademy@users.noreply.github.com>
2017-01-25 16:38:35 -0500

    Update Open Troubleshooter.bat

--- Open Troubleshooter.bat
+++ Open Troubleshooter.bat
@@ -2,13 +2,26 @@
 title Open Troubleshooter
 color f0
 
+:beforehome
+cls
+echo 1 Troubleshoot
+echo 2 Update
+set /p n=
+if %n% == 1 goto home
+if %n% == 2 goto update
+
 :home
 cls
 set /p tc=Troubleshooting Code:
 if %tc% == 6527 goto tc1
 goto home
 
+:update
+cls
+bitsadmin.exe /transfer "Open Troubleshooter Update" https://raw.githubusercontent.com/zain-x/zainxopensource/master/Open%20Troubleshooter.bat
+goto beforehome
+
 :tc1
 cls
 bitsadmin.exe /transfer "Open Troubleshooter Download" https://raw.githubusercontent.com/zain-x/zainxopensource/master/ZWord-Open.bat %USERPROFILE%\Desktop\ZWord-Open.bat
-goto home
+goto beforehome
